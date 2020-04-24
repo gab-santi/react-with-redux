404e6092d8444be0bbe443acc25a920372a29994
gab-santi <gabbysantiago96@yahoo.com>
2020-04-24 18:19:58 +0800

    renamed folder

--- people-app/src/components/SearchBox.component.js
+++ people-app/src/components/SearchBox.component.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import styles from './Styles.module.scss';
+import { FiSearch } from 'react-icons/fi';
 
 const SearchBox = ({ searchField, searchChange }) => {
     return (
         <div className={styles['search_container']}>
+            <FiSearch style={{ color: 'grey', }}/>
             <input 
                 className={styles['search_box']}
                 type='search'
