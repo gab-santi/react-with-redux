6dc520e66defd776a51707825dd938b002e97280
gab-santi <gabbysantiago96@yahoo.com>
2020-04-24 21:42:07 +0800

    Updates

---
+++ precache-manifest.036c476c8cd9e00674b70c2f23bf891b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d3eb6c627bdd29dfdcf5501bebc692",
+    "url": "/react-with-redux/index.html"
+  },
+  {
+    "revision": "60ccf8d6476c7a225f9f",
+    "url": "/react-with-redux/static/css/main.68a5673d.chunk.css"
+  },
+  {
+    "revision": "547eeba051ed18167342",
+    "url": "/react-with-redux/static/js/2.337abc47.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-with-redux/static/js/2.337abc47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ccf8d6476c7a225f9f",
+    "url": "/react-with-redux/static/js/main.801eb2b9.chunk.js"
+  },
+  {
+    "revision": "69e5b01cae308503e81c",
+    "url": "/react-with-redux/static/js/runtime-main.9bddb47c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-with-redux/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0530c3594de539cbfe97e2bffd881e70",
+    "url": "/react-with-redux/static/media/man-3.0530c359.png"
+  }
+]);
